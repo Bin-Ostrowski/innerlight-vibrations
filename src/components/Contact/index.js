efaa3973612f84f2f4dd268ac0c04b849954c2da
Robin Ostrowski <104393956+Bin-Ostrowski@users.noreply.github.com>
2022-10-28 13:22:02 -0700

    contage page now emails formData on submit

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,124 +1,171 @@
 import React, { useState } from "react";
-import { validateEmail } from "../../utils/helpers";
 import { Button, Form, Container, Row, Col } from "react-bootstrap";
 
 export default function ContactForm() {
-  //declare formState to be empty strings
-  const [formState, setFormState] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
+  //declare formData state to be empty object
+  const [formData, setFormData] = useState({});
 
-  //deconstruct formState
-  const { name, email, message } = formState;
-
-  //declare error message for validator to an empty string
-  const [errorMessage, setErrorMessage] = useState("");
+  // declare validation state
+  const [validated, setValidated] = useState(false);
 
   //form input handler
   function handleChange(e) {
-    //validate email, name and message
-    if (e.target.name === "email") {
-      const isValid = validateEmail(e.target.value);
+    const { name, value } = e.target;
 
-      //isvalid conditional statement
-      if (!isValid) {
-        setErrorMessage("Please enter a valid email.");
-      } else {
-        setErrorMessage("");
-      }
-    } else {
-      if (!e.target.value.length) {
-        setErrorMessage(`${e.target.name} is required`);
-      } else {
-        setErrorMessage("");
-      }
-    }
-
-    //conditional statment for error message
-    //update setFormState
-    //make property name dynamic variable determined by form element.
-    //from input value
-    if (!errorMessage) {
-      setFormState({ ...formState, [e.target.name]: e.target.value });
-    }
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   }
 
-  // //submit form handler
-  // function handleSubmit(e) {
-  //   e.preventDefault();
-  //   console.log(formState);
-  // }
+  //submit form handler
+  function handleSubmit(event) {
+    // validate required inputs
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  }
 
   return (
-    <section id="contact">
+    <section id="contact" className="section">
       <Container>
         <Row className="section-title">
           <Col>
             <h1>Contact Me</h1>
           </Col>
         </Row>
-        <Row>
-          <Col className="md form">
-            <Form
-              action="https://formsubmit.co/bin.ostrowski@gmail.com" //porternich@gmail.com
+        <Form
+          action="https://formsubmit.co/bin.ostrowski@gmail.com" //porternich@gmail.com
+          method="POST"
+          target="_blank"
+          className="form"
+          noValidate
+          validated={validated}
+          onSubmit={handleSubmit}
+        >
+          <Form.Group className="mb-4">
+            <Form.Label htmlFor="interests">
+              What are you interested in?
+            </Form.Label>
+            <div>
+              <Form.Check
+                inline
+                label="Sound Healing"
+                name="interestSoundHealing"
+                type="switch"
+                value="yes"
+                onChange={handleChange}
+                id="selectSoundHealing"
+              />
+              <Form.Check
+                inline
+                label="Counseling"
+                name="interestCounseling"
+                value="yes"
+                onChange={handleChange}
+                type="switch"
+                id="selectCounseling"
+              />
+            </div>
+          </Form.Group>
 
-              method="POST"
-              target="_blank"
-              className="form"
-            >
-              <Form.Group className="mb-3">
+          <Row>
+            <Col md={6}>
+              <Form.Group className="mb-4">
                 <Form.Label htmlFor="name">Name:</Form.Label>
                 <Form.Control
+                  required
                   placeholder="Enter Name"
                   type="text"
-                  defaultValue={name}
+                  value={formData.name}
                   onChange={handleChange}
                   name="name"
                 />
               </Form.Group>
-              <Form.Group className="mb-3">
+            </Col>
+            <Col md={6}>
+              <Form.Group className="mb-4">
                 <Form.Label htmlFor="email">Email address:</Form.Label>
                 <Form.Control
+                  required
                   type="email"
-                  defaultValue={email}
+                  value={formData.email}
                   onChange={handleChange}
                   name="email"
-                  required
                   placeholder="Enter Email"
                 />
                 <Form.Text className="text-muted">
                   We'll never share your email with anyone else.
                 </Form.Text>
               </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label htmlFor="message">Message:</Form.Label>
-                <Form.Control
-                  name="message"
-                  defaultValue={message}
-                  onChange={handleChange}
-                  rows="5"
-                  type="message"
-                  placeholder="Enter Message"
+            </Col>
+          </Row>
+
+          <Row>
+            <Col>
+              <Form.Group className="mb-4">
+                <Form.Label htmlFor="insurance">
+                  What insurance do you have?
+                </Form.Label>
+                <Form.Select
                   required
+                  aria-label="Default select example"
+                  onChange={handleChange}
+                  value={formData.insurance}
+                  name="insurance"
+                >
+                  <option value="no insurance">No Insurance</option>
+                  <option value="BCBS">BlueCross and BlueShield</option>
+                  <option value="cigna">Cigna</option>
+                  <option value="pacificSource">PacificSource</option>
+                  <option value="providence">Providence</option>
+                </Form.Select>
+                <Form.Text className="text-muted">
+                  We only accept the insurances listed above.
+                </Form.Text>
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group className="mb-4">
+                <Form.Label htmlFor="preferredTime">
+                  Preferred date of service?
+                </Form.Label>
+                <Form.Control
+                  type="datetime-local"
+                  name="preferredTime"
+                  id="preferredTime"
+                  value={formData.preferredTime}
+                  onChange={handleChange}
                 />
               </Form.Group>
+            </Col>
+          </Row>
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="message">Message:</Form.Label>
+            <Form.Control
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              rows="5"
+              type="message"
+              placeholder="Enter Message"
+              required
+            />
+            <Form.Text className="text-muted">
+              Please add a comment or discribe what you are going through.
+            </Form.Text>
+          </Form.Group>
 
-              {/* conditional render error message (short circuit for if statment) */}
-              {errorMessage && (
-                <div>
-                  <p className="error-text">{errorMessage}</p>
-                </div>
-              )}
-              <div className="d-grid gap-2">
-                <Button type="submit" variant="light">
-                  Send
-                </Button>
-              </div>
-            </Form>
-          </Col>
-        </Row>
+          <div className="d-grid gap-2">
+            <Button type="submit" variant="light">
+              Send
+            </Button>
+          </div>
+        </Form>
       </Container>
     </section>
   );
